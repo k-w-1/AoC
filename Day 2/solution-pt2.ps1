bf4e5048e675547e65d86e042af167e3f055dc1a
Kevin White <kevin+github@morgan-white.ca>
2022-12-02 14:13:38 -0500

    Initial commit

---
+++ Day 2/solution-pt2.ps1
@@ -0,0 +1,42 @@
+﻿$strategy_guide = Get-Content input.txt
+
+$total_score = 0;
+    
+# A Rock     1
+# B Paper    2
+# C Scissors 3
+# X loose    0
+# Y tie      6
+# Z win      3
+
+foreach($line in $strategy_guide) {
+    switch($line.substring(2,1)) {
+        "X" {
+            $total_score += 0 #outcome score (loose) 
+            switch($line.substring(0,1)) {
+                "A" {$total_score += 3} #rock -> scissors
+                "B" {$total_score += 1} #paper -> rock
+                "C" {$total_score += 2} #scissors -> paper
+            }
+            
+        }
+        "Y" { 
+            $total_score += 3 #outcome score (tie) 
+            switch($line.substring(0,1)) {
+                "A" {$total_score += 1} #rock -> rock
+                "B" {$total_score += 2} #paper -> paper
+                "C" {$total_score += 3} #scissors -> scissors
+            }
+        }
+        "Z" {
+            $total_score += 6 #outcome score (win) 
+            switch($line.substring(0,1)) {
+                "A" {$total_score += 2} #rock -> paper
+                "B" {$total_score += 3} #paper -> scissors
+                "C" {$total_score += 1} #scissors -> rock
+            }
+        }
+    }
+}
+
+echo "Total score: $total_score"
---
+++ Day 2/solution.ps1
@@ -0,0 +1,39 @@
+﻿$strategy_guide = Get-Content input.txt
+
+$total_score = 0;
+    
+# A X 1 Rock
+# B Y 2 Paper
+# C Z 3 Scissors
+
+foreach($line in $strategy_guide) {
+    switch($line.substring(2,1)) {
+        "X" {
+            $total_score += 1 #shape score (rock) 
+            switch($line.substring(0,1)) {
+                "A" {$total_score += 3} #tie
+                "B" {$total_score += 0} #loss
+                "C" {$total_score += 6} #win
+            }
+            
+        }
+        "Y" { 
+            $total_score += 2 #shape score (paper)
+            switch($line.substring(0,1)) {
+                "A" {$total_score += 6} #win
+                "B" {$total_score += 3} #tie
+                "C" {$total_score += 0} #loss
+            }
+        }
+        "Z" {
+            $total_score += 3 #shape score (scissors)
+            switch($line.substring(0,1)) {
+                "A" {$total_score += 0} #loss
+                "B" {$total_score += 6} #win
+                "C" {$total_score += 3} #tie
+            }
+        }
+    }
+}
+
+echo "Total score: $total_score"

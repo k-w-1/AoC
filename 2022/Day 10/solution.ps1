80d891c554a5c7edea9b8b52f2bfceba0787ec4d
Kevin White <kevin+github@morgan-white.ca>
2022-12-10 12:50:41 -0500

    Silver

--- 2022/Day 10/solution.ps1
+++ 2022/Day 10/solution.ps1
@@ -4,6 +4,54 @@ if($DebugPreference -eq 'SilentlyContinue') {
     $input = Get-Content .\test_input.txt
 }
 
-foreach($line in $input) {
+$X = 1
+$program_step = $sum = $pending_instructions = 0
+$display = @( , ' ' * 40*6)
+
+foreach($cycle in 1..241) {
+    #draw screen
+    if(($cycle % 40) -in ($X-1)..($X+1)) {
+        $display[$cycle-1] = '#'
+    } else {
+        $display[$cycle-1] = '.'
+    }
     
+    if($pending_instructions -eq 0) {
+        Write-Debug ("C:{0,3} X:{1,4} - loading instruction {2}" -F $cycle, $X, $input[$program_step])
+        $instruction = $input[$program_step++] -split ' '
+    } else {
+        Write-Debug ("C:{0,3} X:{1,4} - decrementing pending instructions" -F $cycle, $X)
+        $pending_instructions--
+    }
+    if($cycle -in @(20, 60, 100, 140, 180, 220)) {
+        $signal = $X * $cycle
+        $sum += $signal
+        Write-Output ("C:{0,3} X:{1,4} - SPECIAL step: signal:{2}, sum:{3}" -F $cycle, $X, $signal, $sum)
+    }
+    switch($instruction[0]) {
+        'noop' {
+            Write-Debug ("C:{0,3} X:{1,4} - Processing noop." -F $cycle, $X)
+        }
+        'add2' {
+            $X += [int]$instruction[1]
+            Write-Debug ("C:{0,3} X:{1,4} - Processing add2." -F $cycle, $X)
+        }
+        'addx' {
+            Write-Debug ("C:{0,3} X:{1,4} - Processing addx." -F $cycle, $X)
+            $instruction[0] = 'add2'
+            $pending_instructions = 1
+        }
+    }
 }
+
+Write-Output "Sum of special instructions: $sum`n"
+
+$Output = '|'
+for($z=0; $z -lt $display.Length; $z++) {
+    $Output += $display[$z]
+    if((($z+1) % 40) -eq 0) {
+        $Output += "|`n|"
+    }
+}
+
+Write-Output $Output

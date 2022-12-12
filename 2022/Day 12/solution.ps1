eaa79a9e87c30a9598d7a7a78b7a399a2cc5b2f9
Kevin White <kevin+github@morgan-white.ca>
2022-12-12 09:16:10 -0500

    Switching computers

--- 2022/Day 12/solution.ps1
+++ 2022/Day 12/solution.ps1
@@ -4,6 +4,13 @@ if($DebugPreference -eq 'SilentlyContinue') {
     $input = Get-Content .\test_input.txt
 }
 
-foreach($line in $input) {
-    
-}
+$board = @(foreach($line in $input) {
+    @(,$line.toCharArray())
+})
+
+#find start pos
+
+#find next step
+
+#path towards
+

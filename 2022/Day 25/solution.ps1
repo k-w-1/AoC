d7c1fc981353b42f19d5eea74c4fa3cb70716d81
Kevin White <kevin+github@morgan-white.ca>
2022-12-13 22:18:49 -0500

    Fix templates yet again...

--- 2022/Day 25/solution.ps1
+++ 2022/Day 25/solution.ps1
@@ -1,9 +1,18 @@
-if($DebugPreference -eq 'SilentlyContinue') {
-    $input = Get-Content .\input.txt
-} else {
-    $input = Get-Content .\test_input.txt
+class solution {
+    [System.Collections.ArrayList]$data = @()
+
+    solution() {
+        if($global:DebugPreference -eq 'SilentlyContinue') {
+            $puzzle_input = Get-Content .\input.txt
+        } else {
+            $puzzle_input = Get-Content .\test_input.txt
+        }
+        foreach($line in $puzzle_input) {
+        }
+    }
 }
 
-foreach($line in $input) {
-    
-}
+$DebugPreference = 'SilentlyContinue'
+$DebugPreference = 'Continue'
+
+[solution] $solution = [solution]::new()

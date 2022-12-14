3db0b94095455953c1a57191feba6f3d659e2f1d
Kevin White <kevin+github@morgan-white.ca>
2022-12-14 09:13:04 -0500

    WIP

--- 2022/Day 14/solution.ps1
+++ 2022/Day 14/solution.ps1
@@ -1,18 +1,40 @@
 class solution {
-    [System.Collections.ArrayList]$data = @()
+    [System.Collections.ArrayList]$board = @()
+    $puzzle_input = @()
 
     solution() {
-        if($global:DebugPreference -eq 'SilentlyContinue') {
-            $puzzle_input = Get-Content .\input.txt
-        } else {
-            $puzzle_input = Get-Content .\test_input.txt
+        $this.puzzle_input = Get-Content .\input.txt
+        #$this.puzzle_input = Get-Content .\test_input.txt
+        
+    }
+
+    characterize() {
+        [System.Collections.ArrayList]$x_points = @()
+        [System.Collections.ArrayList]$y_points = @()
+
+        foreach($line in $this.puzzle_input) {
+            foreach($pair in ($line -split ' -> ')) {
+                $coord = $pair -split ','
+                [void]$x_points.add($coord[0])
+                [void]$y_points.add($coord[1])
+            }
         }
-        foreach($line in $puzzle_input) {
+        $x = ($x_points | measure -Maximum -Minimum)
+        $y = ($y_points | measure -Maximum -Minimum)
+        write-debug "Minimum x coord is $($x.Minimum), maximum $($x.Maximum)"
+        write-debug "Minimum y coord is $($y.Minimum), maximum $($y.Maximum)"
+    }
+
+    draw_lines() {
+        foreach($line in $this.puzzle_input) {
+            
         }
     }
 }
 
-$DebugPreference = 'SilentlyContinue'
-$DebugPreference = 'Continue'
+#$DebugPreference = 'SilentlyContinue'
+#$DebugPreference = 'Continue'
 
 [solution] $solution = [solution]::new()
+
+$solution.characterize()
